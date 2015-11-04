8473ed053f57ca592bf69e1ebc58ab2cc61b3a3b
Christian Askeland <christian.askeland@sintef.no>
2015-11-04 22:30:11 +0100

    added example code

---
+++ nmExampleClass.cpp
@@ -0,0 +1,21 @@
+/*
+ * nmExampleClass.cpp
+ *
+ *  Created on: Nov 4, 2015
+ *      Author: christiana
+ */
+
+#include "nmExampleClass.h"
+
+namespace nm
+{
+
+ExampleClass::ExampleClass()
+{
+}
+
+ExampleClass::~ExampleClass()
+{
+}
+
+} /* namespace cx */
---
+++ nmExampleClass.h
@@ -0,0 +1,23 @@
+/*
+ * nmExampleClass.h
+ *
+ *  Created on: Nov 4, 2015
+ *      Author: christiana
+ */
+
+#ifndef NMEXAMPLECLASS_H_
+#define NMEXAMPLECLASS_H_
+
+namespace nm
+{
+
+class ExampleClass
+{
+public:
+	ExampleClass();
+	virtual ~ExampleClass();
+};
+
+} /* namespace nm */
+
+#endif /* NMEXAMPLECLASS_H_ */
